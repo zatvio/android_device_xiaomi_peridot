8c8d99b9eb5faccd5753c0398aceb741c91617fc
Arian <arian.kulmer@web.de>
2024-03-14 00:14:05 +0530

    peridot: parts: Import from xiaomi_sdm845-common
    https://github.com/LineageOS/android_device_xiaomi_sdm845-common/tree/e261035d91b34cf0fdf0f012927e06a2bcdb03ed/parts
    
    Change-Id: I442f48780ca020f3e420ee4ccaf8ac07b05457cb
    Signed-off-by: Anush02198 <anush.4376@gmail.com>
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>
    Signed-off-by: ramshell688 <ramonzost68@gmail.com>

--- device.mk
+++ device.mk
@@ -423,6 +423,9 @@ PRODUCT_PACKAGES += \
     WifiOverlayPeridotPOCO \
     WifiOverlayPeridotRedmi
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
