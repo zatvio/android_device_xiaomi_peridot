05935efb179fdc93a956f0a495a1e78c1caa44d8
ramshell688 <ramonzost68@gmail.com>
2024-08-18 20:42:12 +0700

    peridot: Remove old parts for more conflicts

--- device.mk
+++ device.mk
@@ -423,10 +423,6 @@ PRODUCT_PACKAGES += \
     WifiOverlayPeridotPOCO \
     WifiOverlayPeridotRedmi
 
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
